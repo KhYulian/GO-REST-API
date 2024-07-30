87e7316e21dba6450d0a02caaeb6c730c3c65a9a
Yulian Khomechko <khomechkoyulian@gmail.com>
2024-07-30 16:34:26 +0300

    add gin and basic handler

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents) // GET POST PUTE PATCH DELETE etc
+
+	server.Run(":8080") // PORT 8080 on localhost
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Heello!"})
+}
